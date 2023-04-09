8097c1f9959cca116a635a982bbfebce8fe29831
Gabriel Baptista <gabriel.costa.baptista1@gmail.com>
2023-04-09 19:42:31 -0300

    refactor: update getPrice

--- src/model/Installation.java
+++ src/model/Installation.java
@@ -73,10 +73,7 @@ public class Installation extends Service{
 
     @Override
     public double getPrice(){
-        double aux = 0;
-        for(String i : programs){
-            aux+=getProgramPrice();
-        }
+        double aux = getProgramPrice()*programs.size();;
         if (operatingSystem != null){
             aux+=getOperatingSystemPrice();
         }
