536d43dcc1f0e363b05575c78dc62cbd6b3e996d
Gabriel Baptista <gabriel.costa.baptista1@gmail.com>
2023-04-10 12:05:24 -0300

    feat: add constructor

--- src/model/Cleaning.java
+++ src/model/Cleaning.java
@@ -2,6 +2,11 @@ package model;
 
 public class Cleaning extends Service{
 
+    public Cleaning(double price, double cost){
+        setPrice(price);
+        setCost(cost);
+    }
+
     public void addService(){
         this.getPrice();
     }
