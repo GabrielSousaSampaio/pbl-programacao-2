f76e452129d0337ff9b5e3306d0067129a7c54ef
Gabriel Sampaio <bielsampaio05@hotmail.com>
2023-04-16 10:12:52 -0300

    feat: create Administrator and Recepcionist class

---
+++ src/model/Administrator.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Administrator extends User{
+    public Administrator(String name, String email, String password) {
+        super(name, email, password);
+    }
+
+
+}
---
+++ src/model/Recepcionist.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Recepcionist extends User{
+
+    public Recepcionist(String name, String email, String password) {
+        super(name, email, password);
+    }
+
+
+}
