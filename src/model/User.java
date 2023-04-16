c3746bf543e3ab743675d3887add00e3aa085475
Gabriel Sampaio <bielsampaio05@hotmail.com>
2023-04-16 10:13:49 -0300

    feat: user password update

--- src/model/User.java
+++ src/model/User.java
@@ -6,11 +6,13 @@ public abstract class User {
     private int id;
     private String name;
     private String email;
+    private String password;
 
     //Constructor
-    public User(String name, String email){
+    public User(String name, String email,String password){
         this.name = name;
         this.email = email;
+        this.password = password;
     }
 
     //Getters and Setters
@@ -36,5 +38,13 @@ public abstract class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
