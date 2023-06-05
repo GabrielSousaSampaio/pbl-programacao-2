8c96c15e43830b5398e2e494adc05ef673cfc9a5
Gabriel Baptista <gabriel.costa.baptista1@gmail.com>
2023-06-05 01:53:03 -0300

    refactor: update login controller

--- src/main/java/com/uefs/pblprogramacao2/controller/LoginController.java
+++ src/main/java/com/uefs/pblprogramacao2/controller/LoginController.java
@@ -1,16 +1,15 @@
 package com.uefs.pblprogramacao2.controller;
 
 
+import com.uefs.pblprogramacao2.Main;
 import com.uefs.pblprogramacao2.dao.DAO;
 import com.uefs.pblprogramacao2.model.users.Administrator;
 import com.uefs.pblprogramacao2.model.users.Recepcionist;
 import com.uefs.pblprogramacao2.model.users.Technician;
+import com.uefs.pblprogramacao2.model.users.User;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,6 +17,8 @@ import java.util.ResourceBundle;
 import static java.lang.Integer.parseInt;
 
 public class LoginController implements Initializable {
+
+    private User user;
 
     private String[] usersTypeList = {"Administrador","Técnico", "Recepcionista"};
 
@@ -34,45 +35,7 @@ public class LoginController implements Initializable {
     private ChoiceBox<String> choiseUserType;
 
     @FXML
-    private void onLoginButtonClick() {
-        try {
-            if (choiseUserType.getValue() == "Administrador") {
-                Administrator user = DAO.getAdministrator().FindById(Integer.parseInt(idTextField.getText()));
-                if (user.getPassword().equals(passwordField.getText())) {
-                    clearAll();
-                    System.out.println("PASSA PARA TELA DE ADMINISTRADOR");
-                }
-                else {
-                    throw new Exception();
-                }
-            }
-            else if (choiseUserType.getValue() == "Técnico") {
-                Technician user = DAO.getTechnician().FindById(parseInt(idTextField.getText()));
-                if ((user.getPassword()).equals(passwordField.getText())) {
-                    clearAll();
-                    System.out.println("PASSA PARA TELA DE TÉCNICO");
-                }
-                else {
-                    throw new Exception();
-                }
-            }
-            else{
-                Recepcionist user = DAO.getRecepcionist().FindById(parseInt(idTextField.getText()));
-                if ((user.getPassword()).equals(passwordField.getText())) {
-                    clearAll();
-                    System.out.println("PASSA PARA TELA DE RECEPCIONISTA");
-                }
-                else {
-                    throw new Exception();
-                }
-            }
-        }
-        catch (Exception e) {
-            this.errorLabel.setText("Id e/ou senha incorreto(s)");
-            this.idTextField.setText("");
-            this.passwordField.setText("");
-        }
-    }
+    private Button loginButton;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -80,7 +43,42 @@ public class LoginController implements Initializable {
         choiseUserType.getItems().addAll(usersTypeList);
     }
 
-    public void clearAll(){
+    @FXML
+    private void onLoginButtonClick() {
+        try {
+            if (choiseUserType.getValue() == "Administrador") {
+                user = DAO.getAdministrator().FindById(Integer.parseInt(idTextField.getText()));
+                if (user.getPassword().equals(passwordField.getText())) {
+                    clearAll();
+                    Main.changeView("adminMenuScene");
+                } else {
+                    throw new Exception();
+                }
+            } else if (choiseUserType.getValue() == "Técnico") {
+                user = DAO.getTechnician().FindById(parseInt(idTextField.getText()));
+                if ((user.getPassword()).equals(passwordField.getText())) {
+                    clearAll();
+                    System.out.println("PASSA PARA TELA DE TÉCNICO");
+                } else {
+                    throw new Exception();
+                }
+            } else{
+                user = DAO.getRecepcionist().FindById(parseInt(idTextField.getText()));
+                if ((user.getPassword()).equals(passwordField.getText())) {
+                    clearAll();
+                    System.out.println("PASSA PARA TELA DE RECEPCIONISTA");
+                } else {
+                    throw new Exception();
+                }
+            }
+        } catch (Exception e) {
+            this.errorLabel.setText("Id e/ou senha incorreto(s)");
+            this.idTextField.setText("");
+            this.passwordField.setText("");
+        }
+    }
+
+    private void clearAll(){
         this.errorLabel.setText("");
         this.idTextField.setText("");
         this.passwordField.setText("");
